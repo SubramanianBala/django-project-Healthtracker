32ef325221bafe010bdae1d6bda1a3fd7a61e2b3
Balasubramanian <forbsubramani@gmail.com>
2025-01-08 12:08:25 +0530

    Delete CRUD directory

--- CRUD/HealthTracker/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'date_of_birth')
-
